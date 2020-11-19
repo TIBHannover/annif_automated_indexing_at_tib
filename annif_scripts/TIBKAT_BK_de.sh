78954a7e18960b8f4f4e7f183ccdd173404a5152
runnwerth <mila@runnwerth.net>
2020-11-19 12:14:09 +0100

    Alles fertig für annif mit TIBKAT

---
+++ annif_scripts/TIBKAT_BK_de.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ 
+annif_project="TIBKAT_BK_de_OP"
+
+#Load vocabulary BK
+echo "Start loading vocabulary BK"
+annif loadvoc $annif_project vocab/bk.tsv
+echo "Vocabulary BK loaded"
+ 
+#Train loaded vocabulary
+echo "Start training of FTX"
+annif train $annif_project training/TIBIndex/TIBKAT_BK_de_202011/
+echo "Vocabulary BK trained"
+ 
+#Evaluate against gold standard
+annif eval $annif_project goldstandard/TIBIndex/TIBKAT_BK_de_202011/
+
+#Run Web GUI
+echo "Start Web GUI"
+annif run
---
+++ extract_goldstandard.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /home/mila/Annif-corpora/goldstandard/TIBIndex/
+
+while read p; do
+        data_set=$(echo "$p" | cut -f1 -d",")
+
+        data_set_txt=${data_set%.xml}.txt
+        data_set_tsv=${data_set%.xml}.tsv
+
+        cp /home/mila/Annif-corpora/getdump/TIBKAT_BK_de/txt/$data_set_txt /home/mila/Annif-corpora/goldstandard/TIBIndex/TIBKAT_BK_de_202011/.
+        cp /home/mila/Annif-corpora/getdump/TIBKAT_BK_de/tsv/$data_set_tsv /home/mila/Annif-corpora/goldstandard/TIBIndex/TIBKAT_BK_de_202011/.
+
+done <Goldstandard_TIBKAT_BK_de_20201117.csv
