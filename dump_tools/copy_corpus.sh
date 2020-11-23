746da0c0af133df9a069465a1d496b8b0c4dc6bb
runnwerth <mila@runnwerth.net>
2020-11-23 13:07:06 +0100

    TIBKAT with abstracts GND, ger

--- dump_tools/copy_corpus.sh
+++ dump_tools/copy_corpus.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd /home/mila/Annif-corpora/getdump/TIBKAT_BK_de/tsv/
+cd /home/mila/Annif-corpora/collections/TIBKAT_abstracts_GND_de/tsv/
 
 for f in *.tsv ; do
-	cp $f /home/mila/Annif-corpora/training/TIBIndex/TIBKAT_BK_de_202011/.
+	cp $f /home/mila/Annif-corpora/training/TIBIndex/TIBKAT_abstracts_GND_de_202011/.
 done
 
-cd /home/mila/Annif-corpora/getdump/TIBKAT_BK_de/txt/
+cd /home/mila/Annif-corpora/collections/TIBKAT_abstracts_GND_de/txt/
 
 for f in *.txt ; do
-	cp $f /home/mila/Annif-corpora/training/TIBIndex/TIBKAT_BK_de_202011/.
+	cp $f /home/mila/Annif-corpora/training/TIBIndex/TIBKAT_abstracts_GND_de_202011/.
 done
