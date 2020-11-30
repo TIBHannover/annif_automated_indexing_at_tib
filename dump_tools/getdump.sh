4a48f1ce8ce1ac7a468f694df118559e527f0a43
runnwerth <mila@runnwerth.net>
2020-11-30 20:14:33 +0100

    Download dump for LinSearch

--- dump_tools/getdump.sh
+++ dump_tools/getdump.sh
@@ -12,16 +12,18 @@
 #baseurl="https://www.tib.eu/oai/public/repository/open?verb=ListRecords&metadataPrefix=marc_xml&set=collection~tibkat_solr~+language:de+xmlPath:subject/@type=gnd"
 #baseurl="https://getinfo.tib.eu/oai/intern/repository/tib?verb=ListRecords&metadataPrefix=ftx&set=collection~tibkat_solr~+language:de+xmlPath:subject/@type=gnd+abstract:*"
 #baseurl="https://getinfo.tib.eu/oai/intern/repository/tib?verb=ListRecords&metadataPrefix=ftx&set=collection~tibkat_solr~+language:en+xmlPath:subject/@type=gnd+abstract:*"
+#baseurl="https://getinfo.tib.eu/oai/intern/repository/tib?verb=ListRecords&metadataPrefix=ftx&set=collection~tibkat_solr~+language:de+locationCode%3A%28L%20mat%2A%20OR%20LB%20mat%2A%29"
 
-baseurl="https://getinfo.tib.eu/oai/intern/repository/tib?verb=ListRecords&metadataPrefix=ftx&set=collection~tibkat_solr~+language:de+locationCode%3A%28L%20mat%2A%20OR%20LB%20mat%2A%29"
+baseurl="https://getinfo.tib.eu/oai/intern/repository/tib?verb=ListRecords&metadataPrefix=ftx&set=collection~tibkat_solr~+language:de+xmlPath:classification/@classificationProcedure=mapping"
 
 set -e
 
 #cd /home/mila/Annif-corpora/getdump/TIBKAT_GND_de/dump/
 #cd /home/mila/Annif-corpora/getdump/TIBKAT_abstracts_GND_de/dump/
 #cd /home/mila/Annif-corpora/getdump/TIBKAT_abstracts_GND_en/dump/
+#cd /home/mila/Annif-corpora/collections/TIBKAT_LokSys_de/dump
 
-cd /home/mila/Annif-corpora/collections/TIBKAT_LokSys_de/dump
+cd /home/mila/Annif-corpora/collections/TIBKAT_LinSearch_de/dump/
 
 function token {
     xmlstarlet sel -N xmlns="http://www.openarchives.org/OAI/2.0/" -t -v '//_:OAI-PMH/_:ListRecords/_:resumptionToken[text()]' -nl $file
