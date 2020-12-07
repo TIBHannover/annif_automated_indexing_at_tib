3e23d736c7f1c82ac1a18c288e2041008f1871e4
runnwerth <mila@runnwerth.net>
2020-12-07 09:43:15 +0100

    LinSearch with vw-multi

---
+++ annif_scripts/TIBKAT_LinSearch_de_vw-multi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ 
+annif_project="TIBKAT_LinSearch_de_vw-multi"
+
+#Load vocabulary GND
+echo "Start loading LinSearch classification"
+annif loadvoc $annif_project vocab/LinSearch.tsv
+echo "LinSearch classification loaded"
+ 
+#Train loaded vocabulary
+echo "Start training"
+annif train $annif_project training/TIBKAT_LinSearch_de/
+echo "Trained"
+
+#Evaluate against gold standard
+touch $annif_project.txt
+annif eval -r $annif_project.txt $annif_project goldstandard/TIBKAT_LinSearch_de/
