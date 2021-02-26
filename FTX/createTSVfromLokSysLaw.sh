bf60871e18d07d43661ff8f6269d1f0468271e8f
runnwerth <mila@runnwerth.net>
2021-02-26 14:50:20 +0100

    LinSearch SKOS serialisation in Turtle, prep for STWFSA, prep for local classification Law

---
+++ FTX/createTSVfromLokSysLaw.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Extract the Law local classification and write it into a corresponding tsv file
+
+cd /home/mila/Annif-corpora/collections/TIBKAT_LokSys_FBR_de/xml/
+
+for f in *.xml ; do
+
+    cp $f ../tsv/
+
+done
+
+cd ../tsv/
+
+for xml in *.xml ; do
+
+    tsv=${xml%.xml}.tsv
+    trans=${xml%.xml}.trans
+
+    # xmlstarlet sel -T -t -m "//classification[@classificationProcedure='mapping']" -v 'code[text()]' -nl "$xml" >> "$trans"
+
+    # sed -i -e 's/^/\<http:\/\/uricorn.fly\/linsearch\#/' "$trans"
+    sed -i -e 's/$/\>/' "$trans"
+	
+    # awk 'FNR==NR{ urls[$1]=$0 } FNR!=NR { print $2"\t"urls[$1] }' /home/mila/Annif-corpora/vocab/LinSearch.tsv $trans > $tsv
+
+    sed -i 's/^[ \t]*//' $tsv
+
+    echo "$xml converted"
+
+    rm $xml
+    rm $trans
+done
--- dump_tools/split_dump_FTX.sh
+++ dump_tools/split_dump_FTX.sh
@@ -7,27 +7,27 @@
 # ./split_dump_FTX.sh 
 # To be executed in the directory where the respective xml files are kept. There needs to be another directory ../xml.
 
-# echo "Split files"
+echo "Split files"
 
-# for xml in *.xml ; do
+for xml in *.xml ; do
 
-#     xml_split -l 2 -n 7 -e .trans $xml
+    xml_split -l 2 -n 7 -e .trans $xml
 
-# done
+done
 
-# echo "Remove artefacts"
+echo "Remove artefacts"
 
-# find . -type f -name 'oai-[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-0000000.trans' | xargs rm
+find . -type f -name 'oai-[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-0000000.trans' | xargs rm
 
-# echo "Move to appropriate folder"
+echo "Move to appropriate folder"
 
-# for trans_file in *.trans ; do
+for trans_file in *.trans ; do
 
-#     cp $trans_file ../xml
+    cp $trans_file ../xml
 
-# done 
+done 
 
-# cd ../xml
+cd ../xml
 
 echo "Rename files"
 
